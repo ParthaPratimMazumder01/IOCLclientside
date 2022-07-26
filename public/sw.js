1bdfe2d447d731d40334670fe595bbc65c221fbc
ParthaPratimMazumder <67221200+ParthaPratimMazumder01@users.noreply.github.com>
2022-07-26 09:52:16 -0700

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("push", (e) => {
+	const data = e.data.json();
+	console.log("Push Recieved...");
+	self.registration.showNotification(data.title,
+		{
+			body: data.body,
+			icon: "https://i.imgur.com/s9mmJNd.png",
+		});
+});
+
